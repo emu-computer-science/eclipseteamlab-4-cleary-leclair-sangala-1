90e44810e5f623600e5cb8f97a5a645f591cb637
ryancleary <ryancleary@localhost>
2025-02-10 12:53:22 -0500

    Added ask method. Invoked ask() in Driver.

--- src/eclipseGroupProject/Driver.java
+++ src/eclipseGroupProject/Driver.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
 	System.out.println("Our team is getting started");
 	
 	Glad.glad();
+	Glad.ask();
 	Sad.sad();
 	Hungry.hungry();
 }
--- src/eclipseGroupProject/Glad.java
+++ src/eclipseGroupProject/Glad.java
@@ -4,4 +4,8 @@ public class Glad {
 	public static void glad() {
 		System.out.println("We are feeling glad!");
 	}
+	
+	public static void ask() {
+		System.out.println("Are you okay?");
+	}
 }
