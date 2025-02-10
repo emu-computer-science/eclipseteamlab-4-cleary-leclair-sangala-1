821463a3d4ede27398e0e05bd5b641bb97b81fc7
tbooyyy <bleclai1@emich.edu>
2025-02-10 12:46:38 -0500

    added sad.java

--- src/eclipseGroupProject/Driver.java
+++ src/eclipseGroupProject/Driver.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Our team is getting started");
 	
 	Glad.glad();
+	Sad.sad();
 }
 } 
---
+++ src/eclipseGroupProject/Sad.java
@@ -0,0 +1,7 @@
+package eclipseGroupProject;
+
+public class Sad {
+	public static void sad() {
+		System.out.println("We are feeling sad!");
+	}
+}
